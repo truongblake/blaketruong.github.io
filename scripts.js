568447b5391216e65ea78eeeb8f8f7c03b5527a7
Blake Truong <truongblake@gmail.com>
2022-12-30 01:52:51 -0800

    minimial

--- scripts.js
+++ scripts.js
@@ -9,4 +9,13 @@ for(let i=0; i < list.length; i++){
     list[i].className = 'list active';
     }
 }
+let navContent = document.querySelector('.nav-content');
+let menu = document.querySelector('.toggle-menu');
+    menu.onclick = function(){
+    menu.classList.toggle('active');
+    navContent.classList.toggle('active');
+    }
+
+
+
  
